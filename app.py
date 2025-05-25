cfc1485317ee25d8a88dc0056de345bff5cf7af3
13155a1 <ppy040@gmail.com>
2025-05-25 22:55:59 +0900

    feat: implement prediction api

---
+++ app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message:": "PU-Track Model Server"}
+
+model = joblib.load("./model.pkl")
+
+class PredictionInput(BaseModel):
+    avg_air_temp: float
+    avg_air_humid: float
+    cushion_slope: float
+    elapsed_time: float
+
+@app.post("/predict")
+def predict(data: PredictionInput):
+    # 입력 데이터를 모델 형식에 맞게 리스트로 변환
+    X_new = [[
+        data.avg_air_temp,
+        data.avg_air_humid,
+        data.cushion_slope,
+        data.elapsed_time
+    ]]
+    # 예측
+    prediction = model.predict(X_new)
+
+    return {"predicted_remaining_time": prediction[0]}
